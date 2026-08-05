12adb4b542b9ff39b84f434ee7dfd9240c6ca289
seokmin <devhong0016@gmail.com>
2026-08-05 17:09:07 +0900

    feat: 게시글 하나 불러오는 api

---
+++ front/src/api/posts.ts
@@ -0,0 +1,7 @@
+import type {PostResponseDto} from '../types/feed.ts'
+import {apiClient} from './client.ts'
+
+export async function getPost(postId: number): Promise<PostResponseDto> {
+    const response = await apiClient.get<PostResponseDto>(`/posts/${postId}`)
+    return response.data
+}
